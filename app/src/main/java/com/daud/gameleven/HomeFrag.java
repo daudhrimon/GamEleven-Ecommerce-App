1d3ae19f38ccca60a31ec309747af1b823bd4ad9
Daud Hoshen Rimon <daudhrimon@gmail.com>
2022-05-14 22:47:48 +0600

    first commit

--- app/src/main/java/com/daud/gameleven/HomeFrag.java
+++ app/src/main/java/com/daud/gameleven/HomeFrag.java
@@ -3,18 +3,33 @@ package com.daud.gameleven;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeFrag extends Fragment {
+    private RecyclerView textRecycler;
+    private LinearLayoutManager layoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_home, container, false);
 
+        initial(view);
+
         return view;
+    }
+
+    private void initial(View view) {
+        textRecycler = view.findViewById(R.id.textRecycler);
+        textRecycler.setHasFixedSize(false);
+        layoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+        textRecycler.setLayoutManager(layoutManager);
+        textRecycler.setAdapter(new TextBtnAdapter(getContext()));
+
     }
 }
---
+++ app/src/main/java/com/daud/gameleven/TextBtnAdapter.java
@@ -0,0 +1,47 @@
+package com.daud.gameleven;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class TextBtnAdapter extends RecyclerView.Adapter<TextBtnAdapter.TextBtnVH> {
+    private Context context;
+    private String[] textList = {"Hot","Computer & Office","Phone Accessories","Gaming PC","Toys","Hot",
+            "Computer & Office","Phone Accessories","Gaming PC","Toys"};
+
+    public TextBtnAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public TextBtnVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.text_btn_vh,parent,false);
+        return new TextBtnVH(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TextBtnVH holder, int position) {
+        holder.tv.setText(textList[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return textList.length;
+    }
+
+    public class TextBtnVH extends RecyclerView.ViewHolder {
+        private TextView tv;
+        public TextBtnVH(@NonNull View itemView) {
+            super(itemView);
+            tv = itemView.findViewById(R.id.tv);
+        }
+    }
+}
