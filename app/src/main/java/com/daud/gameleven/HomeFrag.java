24746587549de32e0dfa94d68fd4a6e974e48f91
Daud Hoshen Rimon <daudhrimon@gmail.com>
2022-05-14 15:04:16 +0600

    first commit

--- app/src/main/java/com/daud/gameleven/HomeFrag.java
+++ app/src/main/java/com/daud/gameleven/HomeFrag.java
@@ -8,12 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeFragment extends Fragment {
+public class HomeFrag extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.frag_home, container, false);
 
         return view;
     }
