ba4e86dadcf52dcd92b45ffa16e4c137c37dd156
Daud Hoshen Rimon <daudhrimon@gmail.com>
2022-05-18 22:14:14 +0600

    third commit

---
+++ app/src/main/java/com/daud/gameleven/FragSignIn.java
@@ -0,0 +1,20 @@
+package com.daud.gameleven;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragSignIn extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag_signin, container, false);
+
+        return view;
+    }
+}
